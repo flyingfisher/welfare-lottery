5ad9b61756638dfea15b2953cdf9d34ad212a789
yuxiang <yuxiang@yuhongtech.com>
2015-05-19 14:27:29 +0800

    remove mathjs dependency and use file require instead of package require

--- index.js
+++ index.js
@@ -2,13 +2,40 @@
  * Created by fish on 2014/11/27.
  */
 
-_ = require("lodash");
-_.str = require("underscore.string");
-math=require("mathjs");
+_ = require("./node_modules/lodash/index.js");
+_.str = require("./node_modules/underscore.string/index.js");
 require("./src/pick5From15Api");
 require("./src/q3dApi");
 require("./src/sevenBallApi");
 require("./src/twoColorBallApi");
+
+math = {};
+math.combinations = function combinations (n, k) {
+    var max, result, i,ii;
+    
+    var arity = arguments.length;
+    if (arity != 2) {
+      throw new Error('combinations', arguments.length, 2);
+    }
+    
+    if (_.isNumber(n)) {
+      if (n < 0) {
+        throw new TypeError('Positive integer value enpected in function combinations');
+      }
+      if (k > n) {
+        throw new TypeError('k must be less than or equal to n');
+      }
+    
+      max = Math.max(k, n - k);
+      result = 1;
+      for (i = 1; i <= n - max; i++) {
+        result = result * (max + i) / i;
+      }
+      return result;
+    }
+    
+    throw new Error('combinations', typeof n);
+};    
 
 exports.findLotteryByCode = function(code){
     var api = require("./src/baseLotteryApi").lotteryRegistry[code];
